66fadc81baf72d395c86a3eaac258ece7a962499
brian-stars <xingchen_game01@163.com>
2023-03-15 22:20:27 +0800

    video 2 ,2.1 ,3

--- src/main/java/net/Brian_stars/rubymod/BrianRubyMod.java
+++ src/main/java/net/Brian_stars/rubymod/BrianRubyMod.java
@@ -2,7 +2,12 @@ package net.Brian_stars.rubymod;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
@@ -18,7 +23,14 @@ public class BrianRubyMod implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 
-	public static final Item RUBY = new Item(new FabricItemSettings());
+	public static final Block RUBY_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).strength(4.0F,1200.0F).sounds(BlockSoundGroup.STONE).requiresTool());
+	public static final Block RUBY_ORE = new Block(FabricBlockSettings.of(Material.STONE).strength(4.0F,1200.0F).sounds(BlockSoundGroup.STONE).requiresTool());
+
+
+	public static final BlockItem RUBY_BLOCK_ITEM = new BlockItem(RUBY_BLOCK,new FabricItemSettings().group(ItemGroups.RUBY));
+	public static final BlockItem RUBY_ORE_ITEM = new BlockItem(RUBY_ORE,new FabricItemSettings().group(ItemGroups.RUBY));
+
+	public static final Item RUBY = new Item(new FabricItemSettings().group(ItemGroups.RUBY));
 
 	@Override
 	public void onInitialize() {
@@ -27,5 +39,11 @@ public class BrianRubyMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		Registry.register(Registry.ITEM,new Identifier(MOD_ID,"ruby"),RUBY);
+
+		Registry.register(Registry.BLOCK,new Identifier(MOD_ID,"ruby_block"),RUBY_BLOCK);
+		Registry.register(Registry.BLOCK,new Identifier(MOD_ID,"ruby_ore"),RUBY_ORE);
+
+		Registry.register(Registry.ITEM,new Identifier(MOD_ID,"ruby_block"),RUBY_BLOCK_ITEM);
+		Registry.register(Registry.ITEM,new Identifier(MOD_ID,"ruby_ore"),RUBY_ORE_ITEM);
 	}
 }
